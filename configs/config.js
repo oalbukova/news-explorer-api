f4b4541f9a7e435ee528f14025159e52a9b63b4e
Olga Albukova <o.albukova@gmail.com>
2020-10-20 02:27:36 +0300

    fix base

--- configs/config.js
+++ configs/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 const { JWT_SECRET = 'JWT_SECRET', NODE_ENV, DB_URL } = process.env;
-const baseAddress = 'mongodb://localhost:27017/newsexplorerdb';
-//const baseAddress = NODE_ENV === 'production' ? DB_URL : 'mongodb://localhost:27017/newsexplorerdb';
+//const baseAddress = 'mongodb://localhost:27017/newsexplorerdb';
+const baseAddress = NODE_ENV === 'production' ? DB_URL : 'mongodb://localhost:27017/newsexplorerdb';
 const baseOptions = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
