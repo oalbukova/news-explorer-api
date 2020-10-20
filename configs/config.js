b75886ae1c2140cbb8080bf5c1b07c455db6f7dd
Olga Albukova <o.albukova@gmail.com>
2020-10-20 19:28:21 +0300

    ready

--- configs/config.js
+++ configs/config.js
@@ -4,12 +4,12 @@ const { NODE_ENV, PORT = 3000 } = process.env;
 
 module.exports = {
   PORT,
-  DB_URL: (NODE_ENV === 'production') ? process.env.DB_URL : 'mongodb://localhost:27017/newsexplorerdb',
+  DB_URL: (NODE_ENV !== 'production') ? 'mongodb://localhost:27017/newsexplorerdb' : process.env.DB_URL,
   baseOptions: {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
   },
-  JWT_SECRET: (NODE_ENV === 'production') ? process.env.JWT_SECRET : 'JWT_SECRET',
+  JWT_SECRET: (NODE_ENV !== 'production') ? 'JWT_SECRET' : process.env.JWT_SECRET,
 };
