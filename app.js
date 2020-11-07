05fc6d2266dfd7864f32041a7932e9ff02c752ce
Olga Albukova <o.albukova@gmail.com>
2020-11-08 00:21:45 +0300

    cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { DB_URL, PORT, baseOptions } = require('./configs/config');
 const limiter = require('./configs/limiter');
@@ -13,12 +14,26 @@ const errorHandler = require('./middlewares/error-handler');
 const app = express();
 
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: [
+      'http://www.albnews.students.nomoreparties.xyz',
+      'https://www.albnews.students.nomoreparties.xyz',
+      'http://albnews.students.nomoreparties.xyz',
+      'https://albnews.students.nomoreparties.xyz',
+      'https://api.albnews.students.nomoreparties.xyz',
+    ],
+    credentials: true,
+  }),
+);
 app.use(helmet());
 app.use(limiter);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 mongoose.connect(DB_URL, baseOptions);
 app.use(requestLogger); // подключаем логгер запросов
 app.use(routes);
