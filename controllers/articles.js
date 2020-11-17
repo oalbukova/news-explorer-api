d3f55c20356d0d40feaa70d08a914556a7e22340
Olga Albukova <o.albukova@gmail.com>
2020-11-17 17:50:15 +0300

    getArticle controllers

--- controllers/articles.js
+++ controllers/articles.js
@@ -10,6 +10,75 @@ const {
   forbiddenErrMsg,
   successDel,
 } = require('../configs/constants');
+
+const getArticles = (req, res, next) => {
+  Article.find({ owner: req.user._id })
+    .orFail(new NotFoundError({ message: notFoundErrMsg.article }))
+    .populate("user")
+    .then((cards) => res.send({ data: cards }))
+    .catch(next);
+};
+
+const createArticle = (req, res, next) => {
+  const {
+    keyword,
+    title,
+    description,
+    publishedAt,
+    source,
+    url,
+    urlToImage,
+  } = req.body;
+  Article.create({
+    keyword,
+    title,
+    description,
+    publishedAt,
+    source,
+    url,
+    urlToImage,
+    owner: req.user._id,
+  })
+    // вернём записанные в базу данные
+    .then((article) =>
+      res.status(201).send({
+        data: {
+          keyword: article.keyword,
+          title: article.title,
+          description: article.description,
+          publishedAt: article.publishedAt,
+          source: article.source,
+          url: article.url,
+          urlToImage: article.urlToImage,
+        },
+      })
+    )
+    .catch(next);
+};
+
+const findByIdAndRemoveArticle = (req, res, next) => {
+  const currentOwner = req.user._id;
+  Article.findOne({ _id: req.params.articleId })
+    .select("+owner")
+    .orFail()
+    .catch(() => {
+      throw new NotFoundError({ message: notFoundErrMsg.articleId });
+    })
+    .then((article) => {
+      if (String(article.owner) !== currentOwner) {
+        throw new ForbiddenError({ message: forbiddenErrMsg });
+      }
+      return Article.findByIdAndDelete(article._id);
+    })
+    .then(() => res.send({ message: successDel }))
+    .catch(next);
+};
+
+module.exports = {
+  getArticles,
+  createArticle,
+  findByIdAndRemoveArticle,
+};
 /*
 const getArticles = (req, res, next) => {
   Article.find({ owner: req.user._id })
@@ -71,7 +140,7 @@ const findByIdAndRemoveArticle = (req, res, next) => {
     .then(() => res.send({ message: successDel }))
     .catch(next);
 }; */
-
+/*
 const createArticle = (req, res, next) => {
   const {
     keyword,
@@ -144,3 +213,4 @@ module.exports = {
   createArticle,
   findByIdAndRemoveArticle,
 };
+*/
