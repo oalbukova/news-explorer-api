f504fb44763561d5e24122d093b176db2021d83e
Olga Albukova <o.albukova@gmail.com>
2020-11-20 13:08:12 +0300

    cookie

--- controllers/users.js
+++ controllers/users.js
@@ -3,24 +3,19 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const { JWT_SECRET } = require('../configs/config');
 const ConflictError = require('../errors/conflict-err');
-const NotFoundError = require('../errors/not-found-err');
-const { conflictErr, notFoundErrMsg } = require('../configs/constants');
+const { conflictErr, successAuth } = require('../configs/constants');
 
 const getUser = (req, res, next) => {
   User.findById(req.user._id)
-    .then((user) => {
-      if (!user) {
-        throw new NotFoundError({ message: notFoundErrMsg.user });
-      }
-      res.send({
-        data: {
-          email: user.email,
-          name: user.name,
-        },
-      });
-    })
+    .then((user) => res.send({
+      data: {
+        email: user.email,
+        name: user.name,
+      },
+    }))
     .catch(next);
 };
+
 const createUser = (req, res, next) => {
   const { email, password, name } = req.body;
   bcrypt
@@ -37,22 +32,34 @@ const createUser = (req, res, next) => {
     })
     .then((user) => res.status(201).send({
       data: {
-        _id: user._id,
         email: user.email,
         name: user.name,
       },
     }))
     .catch(next);
 };
+
 const login = (req, res, next) => {
   const { email, password } = req.body;
+
   return User.findUserByCredentials(email, password)
     .then((user) => {
       // создадим токен
-      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
-        expiresIn: '7d',
-      });
-      res.send({ token });
+      const token = jwt.sign(
+        { _id: user._id },
+        JWT_SECRET,
+        // NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        {
+          expiresIn: '7d',
+        },
+      );
+      res
+        .cookie('jwt', token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true,
+          sameSite: true,
+        })
+        .send({ message: successAuth });
     })
     .catch(next);
 };
